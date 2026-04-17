224820167ffee9e8c76d68c2dd7438f309d0fc7a
phelipeomelo <phelipe.o.melo@gmail.com>
2026-04-17 01:41:19 -0300

    corrigindo porta para render

--- bot.py
+++ bot.py
@@ -108,5 +108,8 @@ def home():
     return "🤖 Bot rodando 24h com sucesso, PH!"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
